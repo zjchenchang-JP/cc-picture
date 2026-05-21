6c247e3ec31579df04c3f7abb6df9cebb4a516f2
zjchenchang <zjchenchang@gmail.com>
2026-05-21 23:29:11 +0900

    first commit

---
+++ cc-picture-backend/src/main/java/com/zjcc/ccpicturebackend/CcPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zjcc.ccpicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcPictureBackendApplication.class, args);
+    }
+
+}
---
+++ cc-picture-backend/src/test/java/com/zjcc/ccpicturebackend/CcPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjcc.ccpicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
