ec490a719aaa3ba8d5baf4830bc9efaceb947c64
zjchenchang <zjchenchang@gmail.com>
2026-05-23 21:29:15 +0900

    first commit

---
+++ cc-picture-frontend/eslint.config.js
@@ -0,0 +1,44 @@
+import pluginVue from "eslint-plugin-vue";
+import typescriptEslint from "@typescript-eslint/parser";
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+    languageOptions: {
+      parser: typescriptEslint,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      vue: pluginVue,
+      "@typescript-eslint": typescriptEslintPlugin,
+      prettier,
+    },
+    rules: {
+      ...pluginVue.configs["flat/recommended"].rules,
+      ...typescriptEslintPlugin.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
+  },
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: pluginVue.configs.base.parser,
+      parserOptions: {
+        parser: typescriptEslint,
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+];
