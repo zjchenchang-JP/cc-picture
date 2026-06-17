f9514297a75d409c0210e45127ede1e1c0e0dfda
zjchenchang <zjchenchang@gmail.com>
2026-06-17 23:01:08 +0900

    feat:空间模块: 数据库表设计-空间表

--- cc-picture-backend/SQL/create_table.sql
+++ cc-picture-backend/SQL/create_table.sql
@@ -65,3 +65,36 @@ CREATE INDEX idx_reviewStatus ON picture (reviewStatus);
 ALTER TABLE picture
     -- 添加新列
     ADD COLUMN thumbnailUrl varchar(512) NULL COMMENT '缩略图 url';
+
+-- 空间功能模块
+-- 空间表
+-- 级别是可枚举的，因此使用整型来节约空间、提高查询效率;
+-- 除了级别字段外，增加 maxSize 和 maxCount 字段用于限制空间的图片总大小与数量，
+-- 而不是在代码中根据级别读取限额。这样管理员可以单独设置限额，不用完全和级别绑定，利于扩展(单独购买容量包)
+create table if not exists space
+(
+    id         bigint auto_increment comment 'id' primary key,
+    spaceName  varchar(128)                       null comment '空间名称',
+    spaceLevel int      default 0                 null comment '空间级别：0-普通版 1-专业版 2-旗舰版',
+    maxSize    bigint   default 0                 null comment '空间图片的最大总大小',
+    maxCount   bigint   default 0                 null comment '空间图片的最大数量',
+    totalSize  bigint   default 0                 null comment '当前空间下图片的总大小',
+    totalCount bigint   default 0                 null comment '当前空间下的图片数量',
+    userId     bigint                             not null comment '创建用户 id',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    editTime   datetime default CURRENT_TIMESTAMP not null comment '编辑时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除',
+    -- 索引设计
+    index idx_userId (userId),        -- 提升基于用户的查询效率
+    index idx_spaceName (spaceName),  -- 提升基于空间名称的查询效率
+    index idx_spaceLevel (spaceLevel) -- 提升按空间级别查询的效率
+) comment '空间' collate = utf8mb4_unicode_ci;
+
+-- 添加新列
+-- 一张图片只能属于一个空间，可以在图片表 picture 中新增字段 spaceId，实现图片与空间的关联，同时增加索引以提高查询性能
+-- 默认情况下，spaceId 为空，表示图片上传到了公共图库
+ALTER TABLE picture
+    ADD COLUMN spaceId  bigint  null comment '空间 id（为空表示公共空间）';
+-- 创建索引
+CREATE INDEX idx_spaceId ON picture (spaceId);
