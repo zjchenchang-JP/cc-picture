5ff1c29386de0ef5368252de4704aa88614940ae
zjchenchang <zjchenchang@gmail.com>
2026-06-19 23:16:58 +0900

    feat:空间模块: 用户创建空间 功能接口开发

--- cc-picture-backend/src/main/java/com/zjcc/ccpicturebackend/service/impl/SpaceServiceImpl.java
+++ cc-picture-backend/src/main/java/com/zjcc/ccpicturebackend/service/impl/SpaceServiceImpl.java
@@ -1,7 +1,9 @@
 package com.zjcc.ccpicturebackend.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zjcc.ccpicturebackend.exception.BusinessException;
@@ -15,9 +17,14 @@ import com.zjcc.ccpicturebackend.model.enums.SpaceLevelEnum;
 import com.zjcc.ccpicturebackend.model.vo.SpaceVO;
 import com.zjcc.ccpicturebackend.service.SpaceService;
 import com.zjcc.ccpicturebackend.mapper.SpaceMapper;
+import com.zjcc.ccpicturebackend.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 /**
 * @author zjcc
@@ -28,9 +35,54 @@ import javax.servlet.http.HttpServletRequest;
 public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space>
     implements SpaceService {
 
+    @Resource
+    private UserService userService;
+
+    // 编程式事务
+    @Resource
+    private TransactionTemplate transactionTemplate;
+
     @Override
     public long addSpace(SpaceAddRequest spaceAddRequest, User loginUser) {
-        return 0;
+        // 转换请求对象和实体类
+        Space space = new Space();
+        BeanUtils.copyProperties(spaceAddRequest,space);
+        // 填充默认值
+        if (StrUtil.isBlank(spaceAddRequest.getSpaceName())) {
+            space.setSpaceName("默认空间");
+        }
+        if (spaceAddRequest.getSpaceLevel() == null) {
+            space.setSpaceLevel(SpaceLevelEnum.COMMON.getValue());
+        }
+        // 填充数据
+        fillSpaceBySpaceLevel(space);
+        // 数据校验
+        validSpace(space,true);
+        Long userId = loginUser.getId();
+        space.setUserId(userId);
+        // 权限校验 非管理员只能创建普通级别的空间
+        if (SpaceLevelEnum.COMMON.getValue() != spaceAddRequest.getSpaceLevel() && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR, "无权限创建指定级别的空间");
+        }
+        // 针对用户进行加锁 TODO 分布式锁
+        // 加锁 + 事务 的方式实现 同一用户只能创建一个私有空间
+        // 最粗暴的方式是给空间表的 userId 加上唯一索引，但由于后续用户还可以创建团队空间，这种方式不利于扩展
+
+        // 按用户加锁 -- 同一个用户的请求串行（同一用户同时只有一个线程在创建空间），不同用户互不影响
+        // 返回字符串在常量池里的规范实例，相同字符串内容永远返回同一个对象
+        String lock = String.valueOf(userId).intern();
+        synchronized (lock) {
+            Long newSpaceId = transactionTemplate.execute(status -> {
+                boolean exists = this.lambdaQuery().eq(Space::getUserId, userId).exists();
+                ThrowUtils.throwIf(exists, ErrorCode.OPERATION_ERROR, "每个用户只能有1个私有空间");
+                boolean result = this.save(space);
+                ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+                // 返回新插入的数据id 主键回填
+                return space.getId();
+            });
+            // 返回结果是包装类Long，可以做一些处理
+            return Optional.ofNullable(newSpaceId).orElse(-1L);
+        }
     }
 
     @Override
@@ -39,7 +91,7 @@ public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space>
         String spaceName = space.getSpaceName();
         Integer spaceLevel = space.getSpaceLevel();
         SpaceLevelEnum spaceLevelEnum = SpaceLevelEnum.getEnumByValue(spaceLevel);
-        // 创建时
+        // 创建时 前端空间名和空间级别必须传
         if (add) {
             ThrowUtils.throwIf(StrUtil.isBlank(spaceName),ErrorCode.PARAMS_ERROR,"空间名称不能为空");
             ThrowUtils.throwIf(null == spaceLevel,ErrorCode.PARAMS_ERROR,"空间级别不能为空");
