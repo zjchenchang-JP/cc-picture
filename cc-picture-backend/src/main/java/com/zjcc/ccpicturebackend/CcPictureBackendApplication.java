41fd8f6ee63bbbb4a289744ba258d56eda432e52
zjchenchang <zjchenchang@gmail.com>
2026-07-13 23:05:37 +0900

    feat:团队空间图片协同编辑模块: 引入Websocket + WebSocket自定义拦截器 + 开发WebSocket数据模型

--- cc-picture-backend/src/main/java/com/zjcc/ccpicturebackend/CcPictureBackendApplication.java
+++ cc-picture-backend/src/main/java/com/zjcc/ccpicturebackend/CcPictureBackendApplication.java
@@ -1,13 +1,15 @@
 package com.zjcc.ccpicturebackend;
 
+import org.apache.shardingsphere.spring.boot.ShardingSphereAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+// @SpringBootApplication(exclude = {ShardingSphereAutoConfiguration.class}) // 启动类排除依赖,关闭分库分表
 // 开启 Spring 基于 AspectJ 注解的自动代理
-// 加了 exposeProxy = true 后,Spring 会把当前正在执行的代理对象塞进一个 ThreadLocal(AopContext),于是你在目标方法里就能手动把它"捞"出来:
+// 加了 exposeProxy = true 后,Spring 会把当前正在执行的代理对象塞进一个 ThreadLocal(AopContext),在目标方法里就能手动把它"捞"出来:
 @EnableAspectJAutoProxy(exposeProxy = true)
 @EnableAsync // 启动@Async 异步方法
 public class CcPictureBackendApplication {
---
+++ cc-picture-backend/src/main/java/com/zjcc/ccpicturebackend/manager/websocket/WsHandshakeInterceptor.java
@@ -0,0 +1,103 @@
+package com.zjcc.ccpicturebackend.manager.websocket;
+
+import cn.hutool.core.util.ObjUtil;
+import cn.hutool.core.util.StrUtil;
+import com.zjcc.ccpicturebackend.manager.auth.SpaceUserAuthManager;
+import com.zjcc.ccpicturebackend.manager.auth.model.SpaceUserPermissionConstant;
+import com.zjcc.ccpicturebackend.model.entity.Picture;
+import com.zjcc.ccpicturebackend.model.entity.Space;
+import com.zjcc.ccpicturebackend.model.entity.User;
+import com.zjcc.ccpicturebackend.model.enums.SpaceTypeEnum;
+import com.zjcc.ccpicturebackend.service.PictureService;
+import com.zjcc.ccpicturebackend.service.SpaceService;
+import com.zjcc.ccpicturebackend.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * WebSocket 拦截器
+ * WebSocket 连接前需要进行权限校验，如果用户没有团队空间内编辑图片的权限，则拒绝握手。所以定义一个 WebSocket 拦截器实现这个能力。
+ * 由于 HTTP 和 WebSocket 的区别，我们不能在后续收到前端消息时直接从 request 对象中获取到登录用户信息，
+ * 因此也需要通过 WebSocket 拦截器，为即将建立连接的 WebSocket 会话指定一些属性，比如登录用户信息、编辑的图片 id 等。
+ */
+@Component
+@Slf4j
+public class WsHandshakeInterceptor implements HandshakeInterceptor {
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private PictureService pictureService;
+
+    @Resource
+    private SpaceService spaceService;
+
+    @Resource
+    private SpaceUserAuthManager spaceUserAuthManager;
+
+    @Override
+    public boolean beforeHandshake(@NotNull ServerHttpRequest request,
+                                   @NotNull ServerHttpResponse response,
+                                   @NotNull WebSocketHandler wsHandler,
+                                   @NotNull Map<String, Object> attributes) {
+        if (request instanceof ServletServerHttpRequest) {
+            HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
+            // 获取请求参数
+            String pictureId = servletRequest.getParameter("pictureId");
+            if (StrUtil.isBlank(pictureId)) {
+                log.error("缺少图片参数，拒绝握手");
+                return false;
+            }
+            User loginUser = userService.getLoginUser(servletRequest);
+            if (ObjUtil.isEmpty(loginUser)) {
+                log.error("用户未登录，拒绝握手");
+                return false;
+            }
+            // 校验用户是否有该图片的权限
+            Picture picture = pictureService.getById(pictureId);
+            if (picture == null) {
+                log.error("图片不存在，拒绝握手");
+                return false;
+            }
+            Long spaceId = picture.getSpaceId();
+            Space space = null;
+            if (spaceId != null) {
+                space = spaceService.getById(spaceId);
+                if (space == null) {
+                    log.error("空间不存在，拒绝握手");
+                    return false;
+                }
+                if (space.getSpaceType() != SpaceTypeEnum.TEAM.getValue()) {
+                    log.info("不是团队空间，拒绝握手");
+                    return false;
+                }
+            }
+            List<String> permissionList = spaceUserAuthManager.getPermissionList(space, loginUser);
+            if (!permissionList.contains(SpaceUserPermissionConstant.PICTURE_EDIT)) {
+                log.error("没有图片编辑权限，拒绝握手");
+                return false;
+            }
+            // 设置 attributes
+            attributes.put("user", loginUser);
+            attributes.put("userId", loginUser.getId());
+            attributes.put("pictureId", Long.valueOf(pictureId)); // 转换为 Long 类型
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(@NotNull ServerHttpRequest request, @NotNull ServerHttpResponse response, @NotNull WebSocketHandler wsHandler, Exception exception) {
+    }
+}
