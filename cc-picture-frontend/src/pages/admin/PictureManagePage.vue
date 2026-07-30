4ea2f786be105d73d118eb4af197db61e8178c4a
zjchenchang <zjchenchang@gmail.com>
2026-07-30 22:45:03 +0900

    feat: 用户批量传图

--- cc-picture-frontend/src/pages/admin/PictureManagePage.vue
+++ cc-picture-frontend/src/pages/admin/PictureManagePage.vue
@@ -2,9 +2,12 @@
   <div id="pictureManagePage">
     <a-flex justify="space-between">
       <h2>图片管理</h2>
-      <a-button type="primary" href="/add_picture" target="_blank"
-        >+ 创建图片</a-button
-      >
+      <a-space>
+        <a-button type="primary" href="/add_picture" target="_blank">
+        + 创建图片
+      </a-button>
+      <a-button type="primary" href="/add_picture/batch" target="_blank" ghost>+ 批量创建图片</a-button>
+      </a-space>
     </a-flex>
     <!-- 搜索表单 -->
     <a-form layout="inline" :model="searchParams" @finish="doSearch">
--- cc-picture-frontend/src/router/index.ts
+++ cc-picture-frontend/src/router/index.ts
@@ -6,6 +6,7 @@ import UserManagePage from '@/pages/admin/UserManagePage.vue'
 import AddPicturePage from '@/pages/AddPicturePage.vue'
 import PictureManagePage from '@/pages/admin/PictureManagePage.vue'
 import PictureDetailPage from '@/pages/PictureDetailPage.vue'
+import AddPictureBatchPage from '@/pages/AddPictureBatchPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -45,6 +46,11 @@ const router = createRouter({
       name: '图片详情',
       component: PictureDetailPage,
       props: true
+    },
+    {
+      path: '/add_picture/batch',
+      name: '批量创建图片',
+      component: AddPictureBatchPage,
     },
 
     // {
