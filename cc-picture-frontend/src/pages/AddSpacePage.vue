839fad9d939134cf740fbdf57121c1ae5471b2a7
zjchenchang <zjchenchang@gmail.com>
2026-08-01 14:24:45 +0900

    feat:空间模块 更新空间页面，支持创建和修改空间功能 + 添加空间级别展示

--- cc-picture-frontend/src/pages/AddSpacePage.vue
+++ cc-picture-frontend/src/pages/AddSpacePage.vue
@@ -1,10 +1,16 @@
 // 批量上传图片
 <template>
   <div id="addSpacePage">
-    <h2 style="margin-bottom: 16px">批量创建图片</h2>
+    <h2 style="margin-bottom: 16px">
+      {{ route.query?.id ? '修改空间' : '创建空间' }}
+    </h2>
     <a-form layout="vertical" :model="formData" @finish="handleSubmit">
       <a-form-item label="空间名称" name="spaceName">
-        <a-input v-model:value="formData.spaceName" placeholder="请输空间名称" allow-clear/>
+        <a-input
+          v-model:value="formData.spaceName"
+          placeholder="请输空间名称"
+          allow-clear
+        />
       </a-form-item>
       <a-form-item label="空间级别" name="spaceLevel">
         <a-select
@@ -16,24 +22,40 @@
         />
       </a-form-item>
       <a-form-item>
-        <a-button type="primary" html-type="submit" style="width: 100%" :loading="loading">
+        <a-button
+          type="primary"
+          html-type="submit"
+          style="width: 100%"
+          :loading="loading"
+        >
           提交
         </a-button>
       </a-form-item>
     </a-form>
+    <a-card title="空间级别介绍">
+      <a-typography-paragraph>
+        * 目前仅支持开通普通版，如需升级空间，请联系
+        <a href="https://github.com/zjchenchang-JP" target="_blank">CC_JP</a>。
+      </a-typography-paragraph>
+      <a-typography-paragraph v-for="spaceLevel in spaceLevelList">
+        {{ spaceLevel.text }}： 大小 {{ formatSize(spaceLevel.maxSize) }}， 数量
+        {{ spaceLevel.maxCount }}
+      </a-typography-paragraph>
+    </a-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
 import { message } from 'ant-design-vue'
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { SPACE_LEVEL_ENUM, SPACE_LEVEL_OPTIONS } from '@/constants/space'
-import { addSpaceUsingPost } from '@/api/spaceController'
+import { addSpaceUsingPost, getSpaceVoByIdUsingGet, listSpaceLevelUsingGet, updateSpaceUsingPost } from '@/api/spaceController'
+import { formatSize } from '@/utils'
 
 const formData = reactive<API.SpaceAddRequest | API.SpaceUpdateRequest>({
   spaceName: '',
-  spaceLevel: SPACE_LEVEL_ENUM.COMMON,
+  spaceLevel: SPACE_LEVEL_ENUM.COMMON
 })
 // 提交任务状态
 const loading = ref(false)
@@ -44,22 +66,82 @@ const router = useRouter()
  * 提交表单
  */
 const handleSubmit = async (values: any) => {
+  // 据是⁠否⁠有⁠ spaceId 决定是​调用更​新接口​还⁠是创建接⁠口
+  const spaceId = oldSpace.value?.id
   loading.value = true
-  const res = await addSpaceUsingPost({
-    ...formData,
-  })
+  let res
+  // 更新
+  if (spaceId) {
+    res = await updateSpaceUsingPost({
+      id: spaceId,
+      ...formData
+    })
+  } else {
+    // 创建
+    res = await addSpaceUsingPost({
+      ...formData,
+    })
+  }
   // 操作成功
   if (res.data.code === 0 && res.data.data) {
-    message.success('创建空间成功')
-    // 跳转到空间信息详情
+    message.success('操作成功')
+    // 跳转到空间信息详情展示
     router.push({
-      path: `/space/${res.data.data}`,
+      path: `/space/${spaceId ?? res.data.data}`
     })
   } else {
     message.error('创建失败，' + res.data.message)
   }
   loading.value = false
 }
+
+
+/**
+ * 空间级别展示
+ */
+const spaceLevelList = ref<API.SpaceLevel[]>([])
+// 获取空间级别
+const fetchSpaceLevelList = async () => {
+  const res = await listSpaceLevelUsingGet()
+  if (res.data.code === 0 && res.data.data) {
+    spaceLevelList.value = res.data.data
+  } else {
+    message.error('加载空间级别失败，' + res.data.message)
+  }
+}
+
+onMounted(()=> {
+  fetchSpaceLevelList()
+})
+
+/**
+ * 更新空间
+ */
+const route = useRoute()
+const oldSpace = ref<API.SpaceVO>()
+
+// 获取老数据
+const getOldSpace = async () => {
+  // 获取数据
+  const id = route.query?.id
+  if (id) {
+    const res = await getSpaceVoByIdUsingGet({
+      id: id as unknown as number
+    })
+    if (res.data.code === 0 && res.data.data) {
+      const data = res.data.data
+      oldSpace.value = data
+      formData.spaceName = data.spaceName
+      formData.spaceLevel = data.spaceLevel
+    }
+  }
+}
+
+// 页面加载时，请求老数据
+onMounted(() => {
+  getOldSpace()
+})
+
 
 </script>
 
@@ -69,4 +151,3 @@ const handleSubmit = async (values: any) => {
   margin: 0 auto;
 }
 </style>
-
